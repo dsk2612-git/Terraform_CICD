89f91b3d3dcd610c308cd5e2590ed59e8d2e0e77
Sai Krishna <saiaws2612@gmail.com>
2025-04-03 12:42:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "dev" {
     ami = "ami-05fa46471b02db0ce"
     instance_type = "t2.medium"
     tags = {
-      Name = "github-we485bhook"
+      Name = "github-webhook"
     }
 }
